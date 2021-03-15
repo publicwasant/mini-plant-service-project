b7686ccec2c93e79d84b86b9159e5704872afd7a
gold <61011222001@msu.ac.th>
2021-03-15 22:07:50 +0700

    update-env-c

---
+++ api/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    const form = env.form(__dirname + '/form.json');
+    const input = env.input(req);
+
+    // code...
+
+    return res.json(form.output);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const http = require('http');
+const request = require('request');
+const password_hash = require('password-hash');
 const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const express = require('express');
 
-const app = require('./app');
-const config = JSON.parse(fs.readFileSync('./config.json'));
+const app = require('./api/app');
+const database = require('./api/database');
 
-http.createServer(app).listen(process.env.port || config.server.port);
+global.env = {
+    fs: fs,
+    url: url,
+    request: request,
+    password_hash: password_hash,
+    database: database,
+    config: JSON.parse(fs.readFileSync('./config.json')),
+    form: ( path ) => { return JSON.parse(fs.readFileSync(path)) },
+    input: ( req ) => { return {
+        url: url.parse(req.url, true).query,
+        body: req.body
+    }; },
+};
+
+http.createServer(app).listen(process.env.port || env.config.server.port);
 
