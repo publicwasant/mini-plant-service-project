0c7821a414c6b773cd5708c51a75adbf6858e94d
gold <61011222001@msu.ac.th>
2021-09-26 00:56:08 +0700

    update-history

--- api/routes/history/history.js
+++ api/routes/history/history.js
@@ -2,25 +2,83 @@ const express = require('express');
 const router = express.Router();
 
 const alternate = (param) => {
-    if (param.c && !param.p && !param.d && !param.m && !param.y && !param.s && !param.e) {
+    if (param.customer_id) {
         return {
-            sql: "SELECT * FROM history WHERE customer_id=? ORDER BY id DESC",
-            values: [param.c]
+            sql: "SELECT * FROM orderitems WHERE oitem_customer_id=? ORDER BY oitem_id DESC",
+            values: [param.customer_id]
         };
-    } else if (param.c && !param.p && param.d && param.m && param.y && !param.s && !param.e) {
+    } else if (param.product_id) {
         return {
-            sql: "SELECT * FROM history WHERE customer_id=? AND date=? ORDER BY id DESC",
-            values: [param.c, param.d + "-" + param.m + "-" + param.y]
+            sql: "SELECT * FROM orderitems WHERE oitem_product_id=? ORDER BY oitem_id DESC",
+            values: [param.product_id]
         };
-    } else if (param.c && !param.p && !param.d && !param.m && !param.y && param.s && param.e) {
+    } else {
         return {
-            sql: "SELECT * FROM history WHERE customer_id=? AND date between ? AND ? ORDER BY id DESC",
-            values: [param.c, param.s, param.e]
+            sql: "SELECT * FROM orderitems ORDER BY oitem_id DESC",
+            values: []
         };
-    } else if (!param.c && param.p && !param.d && !param.m && !param.y && !param.s && !param.e) {
-    } else if (!param.c && param.p && param.d && param.m && param.y && !param.s && !param.e) {
-    } else if (!param.c && param.p && !param.d && !param.m && !param.y && param.s && param.e) {
     }
+};
+
+const reorganize = (time, items, then) => {
+    const result = [];
+
+    const fetch = (i) => {
+        env.get("/order?id=*", [items[i].oitem_order_id], (orders) => {
+            env.get("/product?id=*", [items[i].oitem_product_id], (products) => {
+                env.get("/user/customer?id=*", [items[i].oitem_customer_id], (customers) => {
+                    items[i] = {
+                        order: orders.status == 1 ? {
+                            id: orders.data[0].id,
+                            type: orders.data[0].type,
+                            date: orders.data[0].date
+                        } : null,
+                        item: {
+                            id: items[i].oitem_id,
+                            amount: items[i].oitem_amount,
+                            product: products.status == 1 ? products.data[0] : null,
+                            customer: customers.status == 1 ? customers.data[0] : null,
+                        },
+                    };
+
+                    if (time.from != null && time.to != null) {
+                        if (between(time.from, time.to, orders.data[0].date))
+                            result.push(items[i]);
+                    } else if (time.date != null) {
+                        if (inDate(time.date, orders.data[0].date))
+                            result.push(items[i]);
+                    } else {
+                        result.push(items[i]);
+                    }
+                    
+                    if (i + 1 < items.length) {
+                        fetch (i + 1);
+                    } else {
+                        then(result);
+                    }
+                });
+            });
+        });
+    };
+
+    fetch(0);
+};
+
+const between = (from, to, date) => {
+    const dFrom = new Date(from);
+    const dTo = new Date(to);
+    const dDate = new Date(date);
+
+    return (dDate <= dTo && dDate >= dFrom);
+};
+
+const inDate = (date1, date2) => {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+
+    return (d1.getFullYear() == d2.getFullYear())
+        && (d1.getMonth() == d2.getMonth())
+        && (d1.getDate() == d2.getDate());
 };
 
 router.get('/', (req, res) => {
@@ -29,7 +87,36 @@ router.get('/', (req, res) => {
 
     const alt = alternate(input.url);
 
-    return res.json(form.output);
+    env.database.query(alt.sql, alt.values, (err, result) => {
+        if (err) {
+            form.output.status = 0;
+            form.output.descript = "เกิดข้อผิดพลาดบางอย่าง!";
+            form.output.error = err;
+            form.output.data = [];
+
+            return res.json(form.output);
+        }
+
+        if (result.length > 0) {
+            reorganize({
+                from: input.url.form,
+                to: input.url.to,
+                date: input.url.date
+            }, result, (items) => {
+                form.output.status = 1;
+                form.output.descript = "พบข้อมูลแล้ว " + items.length + " รายการ";
+                form.output.data = items;
+
+                return res.json(form.output);
+            });
+        } else {
+            form.output.status = 0;
+            form.output.descript = "ไม่พบข้อมูล!";
+            form.output.data = result;
+
+            return res.json(form.output);
+        }
+    });
 });
 
 module.exports = router;
